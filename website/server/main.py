dfd7480da8ac3987ff7568b980143ec405b55107
angus <contact.angusgb@gmail.com>
2026-03-21 10:25:36 +0000

    setup before cloudflare

--- website/server/main.py
+++ website/server/main.py
@@ -12,14 +12,17 @@ app.mount("/stylesheets", StaticFiles(directory=os.path.join(BASE_DIR, "styleshe
 app.mount("/scripts", StaticFiles(directory=os.path.join(BASE_DIR, "scripts")), name="scripts")
 app.mount("/component", StaticFiles(directory=os.path.join(BASE_DIR, "component")), name="component")
 
+
 @app.get("/")
 @app.get("/index.html")
 def index():
     return FileResponse(os.path.join(BASE_DIR, "index.html"))
 
+
 @app.get("/differentiation.html")
 def differentiation():
     return FileResponse(os.path.join(BASE_DIR, "differentiation.html"))
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
