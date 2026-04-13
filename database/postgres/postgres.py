f3be096aedd1e7bcb62bc2f2f8dbde33f0eb26e0
angus <contact.angusgb@gmail.com>
2026-04-13 19:10:08 +0100

    fix

--- database/postgres/postgres.py
+++ database/postgres/postgres.py
@@ -1,9 +1,14 @@
 import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+project_root = Path(__file__).resolve().parent.parent.parent
+load_dotenv(project_root / ".env")
 
 DB_CONFIG = {
-    "host": os.getenv("DB_HOST", "localhost"),
-    "port": os.getenv("DB_PORT", "5432"),
-    "user": os.getenv("DB_USER", "admin"),
-    "password": os.getenv("DB_PASSWORD", "password"),
-    "dbname": os.getenv("DB_NAME", "bronze"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "dbname": os.getenv("DB_NAME"),
 }
